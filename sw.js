f5e5c1e2ddd9b74cc7be74a6f1ad386991ab2244
rodrigodiasb <62857786+rodrigodiasb@users.noreply.github.com>
2025-11-10 08:33:49 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,1 @@
-const CACHE_NAME = 'gymcontrol-material-v1';
-const FILES = ['/', '/index.html', '/css/global.css', '/js/app.js', '/js/db.js', '/manifest.json'];
-self.addEventListener('install', evt => {
-  evt.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)));
-});
-self.addEventListener('fetch', evt => {
-  evt.respondWith(caches.match(evt.request).then(r=>r||fetch(evt.request)));
-});
+self.addEventListener('install',()=>{});
